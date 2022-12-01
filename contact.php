df4df4c830bc42dc6babf5034957dba2c0352bc5
andreabeltrami98 <andrea.beltrami@email.com>
2022-12-01 15:50:48 +0100

    TEST

---
+++ contact.php
@@ -0,0 +1,86 @@
+<html>
+    
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel='stylesheet' href='styles.css'>
+</head>
+    
+<body>
+    
+    <div class='section-menu'>
+        <div class='menu'>
+            <div class='logo'>ANDREA BELTRAMI</div>
+            <div class='navbar'>
+                <ul class='navbar-content'>
+                    <li><a class='navbar-link' href='#'>HOME</a></li>
+                    <li><a class='navbar-link' href='#'>ABOUT ME</a></li>
+                    <li><a class='navbar-link' href='#'>WORK</a></li>
+                    <li><a class='navbar-link' href='#'>SKILL</a></li>
+                </ul>
+                <div class='dropdown'>
+                    <button class='dropbutton'></button>
+                    <div class='dropdown-content'>
+                        <a href='#'>HOME</a>
+                        <a href='#'>ABOUT ME</a>
+                        <a href='#'>WORK</a>
+                        <a href='#'>SKILL</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class='container'>
+        <div class='content-1'>
+            
+               <form action='' method='get'>
+                   <div >Contattami</div>
+            <fieldset class='legacy-form-row'>
+                <input id='sig.'
+                       name='sig.'
+                       type='radio'
+                       value='sig.'/>
+                <label for='sig.' class='radio-label'>Sig</label>
+                <input id='sig.ra'
+                       name='sig.ra'
+                       type='radio'
+                       value='sig.ra' />
+                <label for='sig.ra' class='radio-label'>Sig. ra</label>
+            </fieldset>
+			<div class='form-row'>
+                <label for='nome'>Nome</label>
+                <input id='nome' name='nome' type='text' placeholder='Nome'/>
+            </div>
+            <div class='form-row'>
+                <label for='cognome'>Cognome</label>
+                <input id='cognome' name='cognome' type='text' placeholder='Cognome'/>
+            </div>
+            <div class='form-row'>
+                <label for='email'>Email</label>
+                <input id='email'
+                       name='email'
+                       type='email'
+                       placeholder='tuaemail@esempio.com'/>
+            </div>
+
+            <div class='form-row'>
+                <label for='info'>Messaggio</label>
+                <textarea id='info' name='info' placeholder='Scrivi qui il tuo messaggio'></textarea>
+            </div>
+            
+            <div class='form-row'>
+                <button>Submit</button>
+            </div>
+        </form>
+            
+            
+        </div> 
+    </div>
+    
+    <div class='section-footer'>
+        <div class='footer'><p>Andrea Beltrami 2022</p></div>
+    </div>
+    
+</body>
+
+</html>
