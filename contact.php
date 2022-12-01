f9a094440a6946a117962c58afd375a4cab9441c
andreabeltrami98 <andrea.beltrami@email.com>
2022-12-01 19:19:57 +0100

    test

--- contact.php
+++ contact.php
@@ -1,86 +1,65 @@
-<html>
-    
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel='stylesheet' href='styles.css'>
-</head>
-    
-<body>
-    
-    <div class='section-menu'>
-        <div class='menu'>
-            <div class='logo'>ANDREA BELTRAMI</div>
-            <div class='navbar'>
-                <ul class='navbar-content'>
-                    <li><a class='navbar-link' href='#'>HOME</a></li>
-                    <li><a class='navbar-link' href='#'>ABOUT ME</a></li>
-                    <li><a class='navbar-link' href='#'>WORK</a></li>
-                    <li><a class='navbar-link' href='#'>SKILL</a></li>
-                </ul>
-                <div class='dropdown'>
-                    <button class='dropbutton'></button>
-                    <div class='dropdown-content'>
-                        <a href='#'>HOME</a>
-                        <a href='#'>ABOUT ME</a>
-                        <a href='#'>WORK</a>
-                        <a href='#'>SKILL</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    
-    <div class='container'>
-        <div class='content-1'>
-            
-               <form action='' method='get'>
-                   <div >Contattami</div>
-            <fieldset class='legacy-form-row'>
-                <input id='sig.'
-                       name='sig.'
-                       type='radio'
-                       value='sig.'/>
-                <label for='sig.' class='radio-label'>Sig</label>
-                <input id='sig.ra'
-                       name='sig.ra'
-                       type='radio'
-                       value='sig.ra' />
-                <label for='sig.ra' class='radio-label'>Sig. ra</label>
-            </fieldset>
-			<div class='form-row'>
-                <label for='nome'>Nome</label>
-                <input id='nome' name='nome' type='text' placeholder='Nome'/>
-            </div>
-            <div class='form-row'>
-                <label for='cognome'>Cognome</label>
-                <input id='cognome' name='cognome' type='text' placeholder='Cognome'/>
-            </div>
-            <div class='form-row'>
-                <label for='email'>Email</label>
-                <input id='email'
-                       name='email'
-                       type='email'
-                       placeholder='tuaemail@esempio.com'/>
-            </div>
+<?php
+if(!isset($_POST['submit']))
+{
+	echo "Errore! Devi compilare il form correttamente!";
+}
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
 
-            <div class='form-row'>
-                <label for='info'>Messaggio</label>
-                <textarea id='info' name='info' placeholder='Scrivi qui il tuo messaggio'></textarea>
-            </div>
-            
-            <div class='form-row'>
-                <button>Submit</button>
-            </div>
-        </form>
-            
-            
-        </div> 
-    </div>
-    
-    <div class='section-footer'>
-        <div class='footer'><p>Andrea Beltrami 2022</p></div>
-    </div>
-    
-</body>
+//Validate first
+if(empty($name)||empty($visitor_email)) 
+{
+    echo "Nome e email sono obbligatori!";
+    exit;
+}
 
-</html>
+if(IsInjected($visitor_email))
+{
+    echo "Email errata!";
+    exit;
+}
+
+$email_from = 'a.b.spam@email.com';
+
+$email_subject = "Contact form website";
+
+$email_body = "Nuovo messaggio da $name.\n".
+    "Messaggio:\n $message".
+
+    
+$to = "andrea.beltrami@email.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header('Location: home.html');
+
+
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
+   
+?> 
